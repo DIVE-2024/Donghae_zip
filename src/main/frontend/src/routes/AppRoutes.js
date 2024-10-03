762367b3316499e07eef0d7d1ec57af0b8bb1d2f
Kim Youngjae <minorgeniuss@naver.com>
2024-10-03 16:39:09 +0900

    은학이 작업 파일과 히껑 작업 파일 병합 및 정리

--- src/main/frontend/src/routes/AppRoutes.js
+++ src/main/frontend/src/routes/AppRoutes.js
@@ -6,14 +6,22 @@ import AccommodationList from '../pages/Accommodation/AccommodationList';
 import SignUp from '../pages/SignUp/SignUp';
 import Login from '../pages/Login/Login';
 import LoginSuccess from "../services/LoginSuccess";
+import RestaurantList from "../pages/Restaurant/RestaurantList";
+import RestaurantListByHashtag from "../pages/Restaurant/RestaurantListByHashtag";
 
 const AppRoutes = () => {
     return (
         <Routes>
+            {/* 식당 */}
+            <Route path="/restaurants" element={<RestaurantList />} />
             <Route path="/restaurant/:id" element={<Restaurant />} />
+            <Route path="/restaurants/region/:region" element={<RestaurantList />} />
+            <Route path="/restaurants/:region/hashtag/:hashtag" element={<RestaurantListByHashtag />} />
+            {/* 숙소 */}
             <Route path="/accommodation/:uniqueId" element={<Accommodation />} />
             <Route path="/accommodations" element={<AccommodationList />} />
             <Route path="/accommodations/region/:region" element={<AccommodationList />} />
+            {/* 로그인 및 회원가입 */}
             <Route path="/signup" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
             <Route path="/loginSuccess" element={<LoginSuccess />} />
