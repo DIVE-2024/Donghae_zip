073023a045c5071f1d0f6b7ece3c81980073da88
leeeunhak-123 <121605778+leeeunhak-123@users.noreply.github.com>
2024-10-03 15:16:57 +0900

    식당 필터기능 추가

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -5,19 +5,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Restaurant from './Restaurant/Restaurant';
 import Accommodation from './Accommodation/Accommodation';
 import AccommodationList from './Accommodation/AccommodationList';
+import RestaurantList from './Restaurant/RestaurantList';
+import RestaurantListByHashtag from './Restaurant/RestaurantListByHashtag';
 
 function App() {
     return (
         <Router>
             <Routes>
+                {/* 로그인 페이지 */}
                 <Route path="/login" element={<Login />} />
                 <Route path="/loginSuccess" element={<LoginSuccess />} />
+
+                {/* 특정 식당 페이지 */}
                 <Route path="/restaurant/:id" element={<Restaurant />} />
+
+                {/* 전체 식당 목록 페이지 */}
+                <Route path="/restaurants" element={<RestaurantList />} />
+
+                {/* 지역별 식당 목록 페이지 */}
+                <Route path="/restaurants/region/:region" element={<RestaurantList />} />
+
+                {/* 특정 숙박 시설 페이지 */}
                 <Route path="/accommodation/:uniqueId" element={<Accommodation />} />
-                {/* 전체 숙박시설 목록 */}
+
+                {/* 전체 숙박시설 목록 페이지 */}
                 <Route path="/accommodations" element={<AccommodationList />} />
-                {/* 지역별 숙박시설 목록 */}
+
+                {/* 지역별 숙박시설 목록 페이지 */}
                 <Route path="/accommodations/region/:region" element={<AccommodationList />} />
+
+
+                {/* 해시태그별 더보기 페이지 */}
+                <Route path="/restaurants/:region/hashtag/:hashtag" element={<RestaurantListByHashtag />} />
             </Routes>
         </Router>
     );
--- src/main/java/com/example/donghae_zip/repository/RestaurantRepository.java
+++ src/main/java/com/example/donghae_zip/repository/RestaurantRepository.java
@@ -5,6 +5,8 @@ import com.example.donghae_zip.domain.Restaurant;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,12 +16,30 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
     // 지역과 해시태그로 식당 데이터 가져오기 (부분 일치 검색)
     Page<Restaurant> findByRegionAndHashtagContaining(Region region, String hashtag, Pageable pageable);
 
+    // 지역으로 식당 데이터 가져오기
+    Page<Restaurant> findByRegion(Region region, Pageable pageable);
+
     // 해시태그로 식당 데이터 가져오기
     Page<Restaurant> findByHashtag(String hashtag,Pageable pageable);
+
+    // 지역에 따른 구/군 목록 중복 없이 가져오기
+    @Query("SELECT DISTINCT r.district FROM Restaurant r WHERE r.region = :region")
+    List<String> findDistinctDistrictsByRegion(@Param("region") Region region);
 
     // 지역과 구/군, 해시태그로 식당 데이터 가져오기 (부분 일치 검색)
     Page<Restaurant> findByRegionAndDistrictAndHashtagContaining(Region region, String district, String hashtag, Pageable pageable);
 
     // 지역과 구/군으로 식당 데이터 가져오기 (구/군 필터링)
     Page<Restaurant> findByRegionAndDistrict(Region region, String district, Pageable pageable);
+
+    // 지역과 해시태그에 따른 상위 n개의 음식점 가져오기 (정확한 해시태그 검색)
+    List<Restaurant> findTop5ByRegionAndHashtag(Region region, String hashtag, Pageable pageable);
+
+    // 지역과 해시태그에 따른 모든 식당 가져오기 (페이징 처리)
+    Page<Restaurant> findAllByRegionAndHashtag(Region region, String hashtag, Pageable pageable);
+
+    // 지역과 해시태그에 따른 식당의 전체 개수 가져오기
+    int countByRegionAndHashtag(Region region, String hashtag);
+
+
 }
