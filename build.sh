f86d60db745965f632fa55b7087c1d1eac6900ad
leeeunhak-123 <121605778+leeeunhak-123@users.noreply.github.com>
2025-01-23 15:36:35 +0900

    build.sh syntax error 수정

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e  # 오류 발생 시 스크립트 종료
 
 # 1. 프론트엔드 빌드
 echo "Starting frontend build..."
@@ -12,8 +13,7 @@ echo "Copying built files to backend static directory..."
 cp -r build ../resources/static
 
 # 3. 백엔드 빌드
-set -e
-Starting backend build...
+echo "Starting backend build..."
 cd ../../../
 ./gradlew build -x test
 
